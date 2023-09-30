c5be15176a46e3f3f5d7c7221ede05fb5e862b23
Bode Falade <bode.falade@outlook.com>
2023-09-29 22:58:08 -0500

    added router and router receiver functions.

--- main.go
+++ main.go
@@ -10,7 +10,27 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
+// 	switch r.URL.Path {
+// 	case "/":
+// 		homeHandler(w, r)
+// 	case "/contact":
+// 		contactHanlder(w, r)
+// 	default:
+// 		http.Error(w, "Page not found", http.StatusNotFound)
+// 	}
+// 	// fmt.Fprint(w, r.URL.Path)
+// }
+
+func contactHanlder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utl-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1>")
+}
+
+// have access to diff fields that we can set.
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
@@ -19,17 +39,10 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
-	// fmt.Fprint(w, r.URL.Path)
-}
-
-func contactHanlder(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utl-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
-	http.HandleFunc("/contact", contactHanlder)
+	var router Router
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }
