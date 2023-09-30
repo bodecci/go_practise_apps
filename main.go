310fe5abafc0f77ee354b6a500d8b2e5fff19286
Bode Falade <bode.falade@outlook.com>
2023-09-29 21:40:10 -0500

    added path handler.

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utl-8")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utl-8")
 	fmt.Fprint(w, "<h1>Welcome to my Awesome site!</h1>")
 }
 
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
+}
+
+func contactHanlder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utl-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1>")
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/", pathHandler)
+	http.HandleFunc("/contact", contactHanlder)
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
